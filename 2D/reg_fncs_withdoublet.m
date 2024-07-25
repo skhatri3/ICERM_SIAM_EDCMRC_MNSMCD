907f48276cd33df6bd091092b4fe7f81dd0f59e5
kkurianski <86570405+kkurianski@users.noreply.github.com>
2024-07-25 10:57:03 -0400

    Add files via upload
    Updated H1, H2, S1, S2 for case 4. The old one was very wrong lol

--- 2D/reg_fncs_withdoublet.m
+++ 2D/reg_fncs_withdoublet.m
@@ -1,4 +1,4 @@
-function [H1, H2, S1, S2, Q] = reg_fncs_withdoublet(ep, R, blob_num)
+function [H1, H2, S1, S2] = reg_fncs_withdoublet(ep, R, blob_num)
 
 % Computes two components of blobs (aka H1 and H2 in RC's notes) 
 % for the Method of Regularized Stokeslets in 2D 
@@ -41,13 +41,12 @@ switch blob_num
         % psi from Cortez Fluids 2021
         % psi = 2*ep^4*(r^4 - 10*ep^2*r^2 + 5*ep^4)/(Pi*(r^2 + ep^2)^5)
         % From Mathematica:
-        %   H1 = -((-11 d^6 + 9 d^4 r^2 + 7 d^2 r^4 + 3 r^6 + 3 (d^2 + r^2)^3 Log[d^2 + r^2])/(6 (d^2 + r^2)^3))
-        %   H2 = (15 d^4 + 10 d^2 r^2 + 3 r^4)/(3 (d^2 + r^2)^3)
-        %   S1 = (10 d^6 + 5 d^4 r^2 + 4 d^2 r^4 + r^6)/(2 \[Pi] (d^2 + r^2)^4)
-        %   S2 = -((35 d^6 + 7 d^4 r^2 + 5 d^2 r^4 + r^6)/(\[Pi] (d^2 + r^2)^5))
-        r2 = R.^2 - ep^2;
-        H1 = -(-11*ep^6 + 9*ep^4*r2 + 7*d^2*r2.^2 + 3*r2.^3 + 3*R.^3.*log(R.^2))./(6*R.^6);
-        H2 = (15*ep^4 + 10*ep^2*r2 + 3*r2.^2)./(3*R.^6);
-        S1 = (10*ep^6 + 5*ep^4*r2 + 4*ep^2*r2.^2 + r2.^3)./(2*pi*R.^8);
-        S2 = -(35*ep^6 + 7*ep^4*r2 + 5*ep^2*r2.^2 + r2.^3)./(pi*R^(10));
+        %   H1 = (8 d^6 - 2 d^4 R^2 + d^2 R^4 - 3 R^6 Log[R])/(3 R^6)
+        %   H2 = (8 d^4 + 4 d^2 R^2 + 3 R^4)/(3 R^6)
+        %   S1 = (8 d^6 + d^2 R^4 + R^6)/(2 \[Pi] R^8)
+        %   S2 = -((32 d^6 + 2 d^2 R^4 + R^6)/(\[Pi] R^10))
+        H1 = (8*ep^6 - 2*ep^4*R.^2 + ep^2*R.^4 - 3*R.^6*log(R))./(3*R.^6);
+        H2 = (8*ep^4 + 4*ep^2*R.^2 + 3*R.^4)./(3*R.^6);
+        S1 = (8*ep^6 + ep^2*R.^4 + R.^6)./(2*pi*R.^8);
+        S2 = -((32*ep^6 + 2*ep^2*R.^4 + R.^6)./(pi*R.^10));
 end
