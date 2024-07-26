5e3c8bea25bf67b27272c81c7296ee0f64c23f04
kkurianski <86570405+kkurianski@users.noreply.github.com>
2024-07-26 11:50:46 -0400

    Add files via upload
    Update to case 4 blob computations

--- 2D/reg_fncs_withdoublet.m
+++ 2D/reg_fncs_withdoublet.m
@@ -23,17 +23,17 @@ switch blob_num
         r2=R.^2-ep^2;
         S1=-ep^3./(2*pi*r2.*R.^3);
         S2=ep^3*(2*ep^2+5*r2)./(2*pi*r2.^2.*R.^5);
-        % Q=-15*ep^3./(2*pi*R.^7);
-    
+        Q=-15*ep^3./(2*pi*R.^7);
     case 2
-        %a more commonly used blob by RC 
+        %a more commonly used blob by RC (for phi from RC Fluids 2021) 
         % Min error roughly when ep = 0.95*ds
-        r2 = R.^2 - ep^2;
+        % From Mathematica:
+        %   S1 = (d^2 + R^2)/(2 \[Pi] R^4)
+        %   S2 = -((2 d^2 + R^2)/(\[Pi] R^6))
         H1 = -log(R) +  ep^2./R./R;
         H2 = 1./R./R; 
-        S1=-((r2+2*ep^2)./(2*pi*(r2+ep^2).^2));
-        S2=-(-(r2+3*ep^2)./(pi*(r2+ep^2).^3));
-    
+        S1 = (ep^2 + R.^2)./(2*pi*R.^4);
+        S2 = -((2*ep^2 + R.^2)./(pi*R.^6));
     case 3
         % Mystery blob from RC on 7/23
         % Min error roughly when ep = 1.75*ds
@@ -44,18 +44,12 @@ switch blob_num
         % psi from Cortez Fluids 2021
         % psi = 2*ep^4*(r^4 - 10*ep^2*r^2 + 5*ep^4)/(Pi*(r^2 + ep^2)^5)
         % From Mathematica:
-        %   H1 = -((-11 d^6 + 9 d^4 r^2 + 7 d^2 r^4 + 3 r^6 + 3 (d^2 + r^2)^3 Log[d^2 + r^2])/(6 (d^2 + r^2)^3))
-        %   H2 = (15 d^4 + 10 d^2 r^2 + 3 r^4)/(3 (d^2 + r^2)^3)
-        %   S1 = (10 d^6 + 5 d^4 r^2 + 4 d^2 r^4 + r^6)/(2 \[Pi] (d^2 + r^2)^4)
-        %   S2 = -((35 d^6 + 7 d^4 r^2 + 5 d^2 r^4 + r^6)/(\[Pi] (d^2 + r^2)^5))
-        % r2 = R.^2 - ep^2;
-        % H1 = -(-11*ep^6 + 9*ep^4*r2 + 7*ep^2*r2.^2 + 3*r2.^3 + 3*R.^3.*log(R.^2))./(6*R.^6);
-        % H2 = (15*ep^4 + 10*ep^2*r2 + 3*r2.^2)./(3*R.^6);
-        % S1 = (10*ep^6 + 5*ep^4*r2 + 4*ep^2*r2.^2 + r2.^3)./(2*pi*R.^8);
-        % S2 = -(35*ep^6 + 7*ep^4*r2 + 5*ep^2*r2.^2 + r2.^3)./(pi*R.^(10));
-
-        H1=8/3*ep^6./R.^6-2/3*ep^4./R.^4+ep^2./R.^2/3-1/2*log(R.^2);
-        H2=8/3*ep^4./R.^6+4/3*ep^2./R.^4+1./R.^2;
-        S1=-(4/pi*ep^6./R.^8+ep^2./(2*pi*R.^4)+1./(2*pi*R.^2));
-        S2=-(-32*ep^6./(pi*R.^(10))-2*ep^2./(pi*R.^(6))-1./(pi*R.^4));
+        %   H1 = (8 d^6 - 2 d^4 R^2 + d^2 R^4 - 3 R^6 Log[R])/(3 R^6)
+        %   H2 = (8 d^4 + 4 d^2 R^2 + 3 R^4)/(3 R^6)
+        %   S1 = (8 d^6 + d^2 R^4 + R^6)/(2 \[Pi] R^8)
+        %   S2 = -((32 d^6 + 2 d^2 R^4 + R^6)/(\[Pi] R^10))
+        H1 = (8*ep^6 - 2*ep^4*R.^2 + ep^2*R.^4 - 3*R.^6*log(R))./(3*R.^6);
+        H2 = (8*ep^4 + 4*ep^2*R.^2 + 3*R.^4)./(3*R.^6);
+        S1 = (8*ep^6 + ep^2*R.^4 + R.^6)./(2*pi*R.^8);
+        S2 = -((32*ep^6 + 2*ep^2*R.^4 + R.^6)./(pi*R.^10));
 end
