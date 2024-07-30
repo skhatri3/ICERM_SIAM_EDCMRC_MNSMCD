2caf4ffe731ff8552eb2a239837972cecc33df9f
mkuback1 <67384433+mkuback1@users.noreply.github.com>
2024-07-30 17:54:55 -0400

    Add files via upload

--- 3D/reg_fncs.m
+++ 3D/reg_fncs.m
@@ -1,6 +1,6 @@
 function [H1, H2, S1, S2, Q] = reg_fncs(ep, R, blob_num)
 
-% Computes two components of blobs 
+% Computes 5 components of blobs 
 % for the Method of Regularized Stokeslets in 3D 
 % Based on Cortez, Fauci, Medovikov, Physics of Fluids 2005 
 
@@ -12,17 +12,6 @@ function [H1, H2, S1, S2, Q] = reg_fncs(ep, R, blob_num)
 %R: distance between targe and source point + regularization 
 %   (R = sqrt(|x-y|^2 + ep^2))
 
-% Blob as given in Cortez, Fauci, Medovikov, Physics of Fluids 2005, Eqn 9 
-A = (R.^2 + ep.^2) ./ (R.^3);
-B = 1./ (R.^3);
-
-% Another blob in 3D 
-%R2 = R.^2; 
-%R3 = R.^3; 
-%d2 = ep.^2; 
-
-%B = (2*R3.^2 +3*d2*R2.^2+35*d2^3)./(2*R3.^3) ;
-%A = (R2 + d2).*B -2*d2*(15*d2^2+R2.^2)./(R3.*R2.^2) ;
 r2 = R.^2 - ep^2;
 
 switch blob_num
@@ -33,6 +22,16 @@ switch blob_num
         S1 = (3*ep^2 + 2*R.^2) ./ (8*pi*R.^5);
         S2 = -3*(5*ep^2 + 2*R.^2) ./ (8*pi*R.^7);
         Q = -105*ep^4 ./ (8*pi*R.^9);
-   %case 2
-        
+   case 2
+        % blob = (15*d^4 (40*d^6 - 132*d^4*r^2 + 57*d^2*r^4 - 2*r^6))...
+        %          /(2*(d^2 + r^2)^(13/2))
+        H1 = (35*ep^8 - 25*ep^6*R.^2 + 3*ep^4*R.^4 + ep^2*R.^6 ...
+            + 2*R.^8)./(2*R.^9);
+        H2 = (35*ep^6 + 3*ep^2*R.^4 + 2*R.^6)./(2*R.^9);
+        S1 = (315*ep^8 - 140*ep^6*R.^2 + 15*ep^4*R.^4 ...
+            + 6*ep^2*R.^6 + 4*R.^8)./(2*R.^11);
+        S2 = -3*(1155*ep^8 - 420*ep^6*R.^2 + 35*ep^4*R.^4 ...
+            + 10*ep^2*R.^6 + 4*R.^8)./(2*R.^13);
+        Q = 15*ep^4*(-3003*ep^6 + 3012*ep^4*R.^2 - 12*r2.^2*R.^2 ...
+            + 26*R.^6 + 3*ep^2*(76*r2.*R.^2 - 273*R.^4))./(2*R.^15);
 end
