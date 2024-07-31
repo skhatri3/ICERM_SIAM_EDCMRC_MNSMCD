590d7296e55a9d9a214ff85c720f3ee5d94430bd
kkurianski <86570405+kkurianski@users.noreply.github.com>
2024-07-31 11:19:23 -0400

    Add files via upload
    Updated naming conventions to match Shilpa's permeable infinite channel code

--- 2D/main_channel.m
+++ 2D/main_channel.m
@@ -1,15 +1,14 @@
 %main_channel
 
-%Setting up Poiseuille flow velocity in channel and computing the
-%force at given points 
+%Poiseuille flow velocity in a 2D channel
 
 %Developed by Shilpa Khatri and Ricardo Cortez  
 %July 2024 
 
 %Edited by Kristin Kurianskia and Brittany Leather
 
-%clear all
-%close all 
+clear all
+close all 
 
 %% Parameters to set 
 
@@ -17,59 +16,83 @@
 mu = 1; 
 
 %number of points on boundary where force is applied 
-N = 200;
+N = 100;
 
 %choose blob
-blob_num = 2;
+blob_num = 1;
 
-%% Setting forces and computing velocity 
+%resolution for velocity
+Nx1 = 20*5;
+Nx2 = 20;
+
+% Setting forces and computing velocity 
 
 %channel
-Lx = 10;
+Lx = 5;
 Ly = 1;
-xmin = 0;
-xmax = xmin + Lx;
-ymin = 0;
-ymax = ymin + Ly;
+xmin = -Lx;
+xmax = Lx;
+ymin = -Ly;
+ymax = Ly;
 
-%discretization of channel
-ds = xmax/N;
-s = 0:ds:xmax;
-s = s';
+%discretization step
+ds = (xmax-xmin)/N;
 
 %regularization parameter
-ep = 1.75*ds;
+ep = 1.5*ds;
 
-%top wall coordinates (x,y) = (y1_top,y2_top)
-y1_top = s;
-y2_top = ymax*ones(size(y1_top));
+%discretization of top and bottom
+ds = (xmax-xmin)/N;
+%stb = xmin+ds/2:ds:xmax-ds/2;
+stb = xmin:ds:xmax;
+stb = stb';
 
-%bottom wall coordinates (x,y) = (y1_bot,y2_bot)
-y1_bot = s;
-y2_bot = ymin*ones(size(y1_bot));
+%discretization of left wall
+%slr = ymin+ds/2:ds:ymax-ds/2;
+slr = ymin+ds:ds:ymax-ds;
+slr = slr';
 
-%left-hand wall coordinates (x,y) = (y1_side,y2_side)
-y2_side = (ymin+ds:ds:ymax-ds)';
-y1_side = zeros(size(y2_side));
+%top wall coordinates (y1_top,y2_top)
+y1_top = stb;
+y2_top = ymax*ones(size(stb));
+
+%bottom wall coordinates (y1_bot,y2_bot)
+y1_bot = stb;
+y2_bot = ymin*ones(size(stb));
+
+%left wall coordinates (y1_left,y2_left)
+y1_left = xmin*ones(size(slr));
+y2_left = slr;
+
+%right wall coordinates (y1_right,y2_right)
+y1_right = xmax*ones(size(slr));
+y2_right = slr;
 
 %coordiantes of boundary points
-y1 = [y1_top; y1_bot; y1_side];
-y2 = [y2_top; y2_bot; y2_side];
+y1 = [y1_top; y1_bot; y1_left; y1_right];
+y2 = [y2_top; y2_bot; y2_left; y2_right];
 
 %velocity on boundary
-%top
-u1_top=zeros(size(y1_top));
-u2_top=zeros(size(y1_top));
-%bottom
-u1_bot=zeros(size(y1_bot));
-u2_bot=zeros(size(y1_bot));
-%side
-a=10;
-u1_side=-a*(y2_side-ymin).*(y2_side-ymax);
-u2_side=zeros(size(y1_side));
+%top velocity
+u1_top = zeros(size(stb));
+u2_top = zeros(size(stb));
 
-u1 = [u1_top; u1_bot; u1_side];
-u2 = [u2_top; u2_bot; u2_side];
+%bottom velocity
+u1_bot = zeros(size(stb));
+u2_bot = zeros(size(stb));
+
+%left velocity
+pL=1;
+u_pois = -pL*(y2_left-ymin).*(y2_left-ymax);%/2/mu;
+
+u1_left = u_pois;
+u2_left = zeros(size(slr));
+
+u1_right = u_pois;
+u2_right = zeros(size(slr));
+
+u1 = [u1_top; u1_bot; u1_left; u1_right];
+u2 = [u2_top; u2_bot; u2_left; u2_right];
 
 %computing the force 
 f = RegStokeslets2D_velocitytoforce([y1,y2],[y1,y2],[u1,u2],ep,mu,blob_num);
@@ -80,104 +103,50 @@ f1 = f(:,1);
 f2 = f(:,2);
 
 %Points where velocity will be computed
-x2 = y2_side;
-x1 = 5*ones(size(x2));
-
-%Compute velocity
-u = RegStokeslets2D_forcetovelocity([y1, y2],f,[x1 x2],ep,mu,blob_num);
-
-
-%% Compute velocity everywhere
-%domain on which velocity is computed 
-x1min = 0; 
-x1max = xmax;
-x2min = 0; 
-x2max = ymax; 
-
-%resolution for velocity grid
-Nx1 = 80;  
-Nx2 = 80;
-
-%points on which velocity will be computed 
-xx1 = linspace(x1min,x1max,Nx1);
-xx2 = linspace(x2min,x2max,Nx2); 
-[x1m,x2m] = ndgrid(xx1,xx2); 
+xx1 = linspace(xmin,xmax,Nx1);
+xx2 = linspace(ymin,ymax,Nx2);
+[x1m, x2m] = ndgrid(xx1, xx2);
 x1 = x1m(:);
 x2 = x2m(:);
 
-%computing velocity 
-uu = RegStokeslets2D_forcetovelocity([y1,y2],[f1,f2],[x1,x2],ep,mu,blob_num);
-uu1 = uu(:,1);
-uu2 = uu(:,2); 
-u1m = reshape(uu1,size(xx1,2),size(xx2,2)); 
-u2m = reshape(uu2,size(xx1,2),size(xx2,2));
+%computing velocity everywhere
+ug = RegStokeslets2D_forcetovelocity([y1,y2],[f1,f2],[x1,x2],ep,mu,blob_num);
+ug1 = ug(:,1);
+ug2 = ug(:,2);
+u1m = reshape(ug1,size(xx1,2),size(xx2,2));
+u2m = reshape(ug2,size(xx1,2),size(xx2,2));
+umag = sqrt(u1m.^2 + u2m.^2);
 
-
-%% Compute velocity at top boundary
-%domain on which velocity is computed 
-x1min = 0; 
-x1max = xmax;
-x2min = ymax; 
-x2max = ymax; 
-
-%resolution for velocity grid
-Nx1 = 2000;  
-
-%points on which velocity will be computed 
-xx1 = linspace(x1min,x1max,Nx1)';
-xx2 = ymax*ones(size(xx1));
-
-%computing velocity 
-uu = RegStokeslets2D_forcetovelocity([y1,y2],[f1,f2],[xx1,xx2],ep,mu,blob_num);
-
-figure
-plot(xx1, uu(:,2), '.-k')
-%plot(xx1, sqrt(uu(:,1).^2+uu(:,2).^2), '.k')
-ylim([-0.001,0.001])
-
-
-%% Compute error
-error1 = abs(u(:,1)-u1_side);
-error2 = abs(u(:,2)-u2_side);
-
-max(max(error1))/max(u1_side)
-max(max(error2))
-
-%figure; plot(x1, x2); hold on; quiver(u(:,1), u(:,2))
+%compute pressure
+p = RegStokeslets2D_forcetopressure([y1, y2],f,[x1 x2],ep,blob_num);
+xpresh = xx1;
+pmesh = reshape(p,size(xx1,2),size(xx2,2));
+pmesh = pmesh-min(p);
 
 %% Plotting
-% Plot channel with forces as quiver and velocity profile as dashed line
-figure
+skip = 4;
+
+figure(4)
 plot(y1,y2,'k.')
-hold on 
-quiver(y1,y2,f1,f2,'r')
-
-par = -a*(y2_side-ymin).*(y2_side-ymax);
-plot(par, y2_side, 'b--')
-
+hold on
+quiver(y1,y2,u1,u2,'r')
 axis equal
-xlim([xmin,xmax])
-ylim([ymin,ymax])
 
-title('Forces and Computed Velocity')
-xlabel('$x$', Interpreter='latex')
-ylabel('$y$', Interpreter='latex')
-ax = gca;
-ax.FontSize = 20;
-
-
-% Plot channel with velocity everywhere in channel
-figure
+figure(5)
 plot(y1,y2,'k.')
-surf(x1m, x2m, sqrt(u1m.^2+u2m.^2), 'EdgeColor', 'none')
-view(2)
+hold on
+quiver(y1,y2,f1,f2,'r')
+axis equal
 
-box on
-colorbar 
-axis equal 
-grid off
+figure(1)
+plot(y1,y2,'k.')
+hold on
+pcolor(x1m,x2m,umag)
+shading interp
+quiver(y1,y2,f1,f2,'r')
+axis equal
+quiver(x1m(1:skip:end,1:skip:end),x2m(1:skip:end,1:skip:end),u1m(1:skip:end,1:skip:end),u2m(1:skip:end,1:skip:end),'k')
 xlim([xmin,xmax])
 ylim([ymin,ymax])
-title('Velocity Magnitude')
-xlabel('$x$', Interpreter='latex')
-ylabel('$y$', Interpreter='latex')
+colorbar
+title('Forces and Computed Velocity')
