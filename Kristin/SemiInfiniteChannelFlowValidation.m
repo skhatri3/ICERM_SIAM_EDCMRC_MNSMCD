6840b90dacb48d4f14e9b3dc0a8505220eae7852
kkurianski <86570405+kkurianski@users.noreply.github.com>
2025-06-10 10:18:01 -0700

    Update visualization
    Updated visualization of contour and color bar to match FB paper

--- Kristin/SemiInfiniteChannelFlowValidation.m
+++ Kristin/SemiInfiniteChannelFlowValidation.m
@@ -6,7 +6,7 @@
 %Geometry
 Ninlet = 30; % points per inlet unit length
 
-xm = 0;  xM = pi; % range in x
+xm = 0;  xM = 5; % range in x
 ym = -1;  yM = 1;  % range in y
 
 L = xM-xm;  H = yM-ym; % lengths of x and y domain
@@ -22,7 +22,7 @@ y = (ym+hH/2 : hH : yM-hH/2)';  NH = length(y)-1;
 % Darcy number 
 Da = 0.3; %Da<1/2 has finitely many real eigvals
 
-[uexact, vexact, pexact] = permeablechannelexact_semiinf(xx, yy, Da, L/yM);
+[uexact, vexact, pexact] = permeablechannelexact_semiinf(xx, yy, Da, xM);
 
 % Quiver plot
 figure
@@ -33,18 +33,23 @@ ax.FontSize = 14;
 
 % Plotting streamlines with pressure contour
 figure
-h = contourf(xx, yy, pexact, 12, 'EdgeColor', 'none');
+[conmat, conobj] = contourf(xx, yy, log(abs(pexact)), 'EdgeColor', 'none');
 hold on
+quiver(xx(1:3:end, 1:3:end), yy(1:3:end, 1:3:end), uexact(1:3:end, 1:3:end), vexact(1:3:end, 1:3:end), 2.5);
+%stream_in = streamline(xx, yy, uexact, vexact, xx(1:end, 1), yy(1:end, 1));
 
-stream_in = streamline(xx, yy, uexact, vexact, xx(1:end, 1), yy(1:end, 1));
+%set(stream_in, 'Color', 'black');
+%axis equal;
+%ax = gca;
+%ax.FontSize = 14;
 
-set(stream_in, 'Color', 'black');
-axis equal;
-ax = gca;
-ax.FontSize = 14;
-
-colorbar
-colormap bone
+colorbar('Ticks', -1.1:1.1:2.5)
+colormap(bone(11))
+clim([-1.1-1.1/3, 2.2+1.1/3])
+ax = gca; ax.FontSize = 26;
+axis equal
+xlim([0, 5])
+%clim([min(conobj.LevelList), max(conobj.LevelList)])
 % FB has logarithmic scale for pressure that gives different contours.
 % We think that the discrepancy is that Figure 1 is in dimensional
 % variables while we are using the nondimensional. To do: try using
@@ -53,6 +58,7 @@ colormap bone
 
 %%
 function [usol, vsol, psol] = permeablechannelexact_semiinf(x, y, Da, L_r)
+% L_r = L/r in paper
 
 % solve eq (30) in FB
 f=@(lam) tan(lam)^2 - tan(lam)/lam + 1 - 2*Da;
@@ -60,6 +66,7 @@ lam1 = fzero(f, sqrt(2*Da));
 
 % coefficient from eq (66) of FB
 C1 = (-2*L_r)*lam1/sin(lam1);
+disp(['lambda_1 = ', num2str(lam1)])
 
 % set up g_1(y) and derivative, eq (40) in FB
 g = cot(lam1)*(Da - 0.5)*sin(lam1*y) + 0.5*y.*cos(lam1*y);
