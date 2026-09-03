0f8c780febb45e010d05dff68aa3535d90d7ec40
kkurianski <86570405+kkurianski@users.noreply.github.com>
2026-09-03 16:01:51 -0700

    Saved plots of alpha and lambda

--- Kristin/channelPozrikidis/alphaLambda_params.m
+++ Kristin/channelPozrikidis/alphaLambda_params.m
@@ -52,17 +52,23 @@ ylabel('$|\alpha-\lambda|$', 'Interpreter', 'latex')
 ax = gca; ax.FontSize = 16;
 
 
-figure
-semilogy(Da_vec, lambda_vec, '-', 'LineWidth', 1.3)
-hold on
-semilogy(Da_vec, alpha_vec, '-', 'LineWidth', 1.3)
-xlabel('Da')
-legend('$\lambda$', '$\alpha$', 'interpreter', 'latex', 'location', 'northwest', 'fontsize', 20)
-ax = gca; ax.FontSize = 16;
+% figure
+% semilogy(Da_vec, lambda_vec, '-', 'LineWidth', 1.3)
+% hold on
+% semilogy(Da_vec, alpha_vec, '-', 'LineWidth', 1.3)
+% xlabel('Da')
+% legend('$\lambda$', '$\alpha$', 'interpreter', 'latex', 'location', 'northwest', 'fontsize', 20)
+% ax = gca; ax.FontSize = 16;
 
 
 figure
 semilogy(Da_vec, paramDiffs, '-', 'LineWidth', 1.3)
+xlabel('Da')
+ylabel('$|\alpha-\lambda|$', 'Interpreter', 'latex')
+ax = gca; ax.FontSize = 16;
+
+figure
+loglog(Da_vec, paramDiffs, '-', 'LineWidth', 1.3)
 xlabel('Da')
 ylabel('$|\alpha-\lambda|$', 'Interpreter', 'latex')
 ax = gca; ax.FontSize = 16;
